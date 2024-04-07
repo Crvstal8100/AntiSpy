a51c1a8d7df804374ccfc983f32cc054834ae5e6
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2024-04-07 12:47:17 +0200

    Create Documentation.lua

---
+++ Documentation.lua
@@ -0,0 +1,18 @@
+local AntiSpy = loadstring(game:HttpGet('https://raw.githubusercontent.com/Crvstal8100/AntiSpy/main/Main.lua'))()
+
+--[[
+
+THIS IS NOT IMPORTANT:
+
+FUNCTIONS:
+function AntiSpy:GetPlayerInfo(player: Player) returns the player info {["Flags"], ["LastCFrame"], ["LastMessage"]}
+
+function AntiSpy:GetAccessories(player: Player) returns all the accessories (table)
+
+function AntiSpy:AddFlag(player: Player, flag: string)
+
+function AntiSpy:RemoveFlag(player: Player, flag: string)
+
+function AntiSpy:ClearFlags(player: Player, flag: string)
+
+]]--
